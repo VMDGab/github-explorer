786c4467480b34244403ad5b960f828b25b75a4e
VMDGab <gmatosdantas777@gmail.com>
2024-09-03 13:03:44 -0300

    Defining global css variables

---
+++ src/components/header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+        <img />
+        <button id="btn" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        theme: String
+    },
+     methods(){
+        this.theme == "lightTheme" ? btn.classList.add("light") : btn.classList.add("dark")
+    }
+}
+</script>
+
+<style>
+
+.light{
+    background-color: #c11;
+}
+
+.dark{
+    background-color: #000;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home lightTheme">
+    <Header theme="lightTheme"/>
   </div>
 </template>
-
+<style src="../css/home.css"></style>
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/header.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
